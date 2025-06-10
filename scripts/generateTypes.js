89a705e0c6277f6537f8b88ae19aedf0f016a5b2
Swapnil agarwal <swapnilag@google.com>
2025-06-10 14:05:18 -0700

    feat: generate types from spec & use unknown in types

---
+++ scripts/generateTypes.js
@@ -0,0 +1,25 @@
+import { compile, compileFromFile } from 'json-schema-to-typescript'
+import fs from 'fs';
+import path from 'path';
+
+function clearAndUpper(text) {
+  return text.replace(/-/, "").toUpperCase();
+}
+
+function toPascalCase(text) {
+  return text.replace(/(^\w|-\w)/g, clearAndUpper);
+}
+
+const typeSchemaContents = fs.readFileSync(path.join(process.cwd(), 'spec.json'), 'utf8');
+const typeSchema = JSON.parse(typeSchemaContents.toString());
+
+// Manually injecting TaskState enum names in pascal case.
+typeSchema.definitions.TaskState["tsEnumNames"] = typeSchema.definitions.TaskState.enum.map(name => toPascalCase(name))
+
+compile(typeSchema, 'MySchema', {
+    additionalProperties: false,
+    enableConstEnums: false,
+    unreachableDefinitions: true,
+    unknownAny: true,
+  })
+  .then(ts => fs.writeFileSync('src/types.ts', ts))
